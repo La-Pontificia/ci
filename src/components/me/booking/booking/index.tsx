48884251fdb7348baea0454590e431707db083f9
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2024-01-22 09:19:47 -0500

    Add qr component

--- src/components/me/booking/booking/index.tsx
+++ src/components/me/booking/booking/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { type Booking as BookingType } from 'types'
 
 import { convertFormatHour, formatSpanishDate } from 'herpers'
-// import Qr from './qr'
+import Qr from './qr'
 import { CalendarIcon, ClockIcon, DisplayIcon, TableIcon } from 'icons'
 import DropDownBooking from './dropdown'
 import { cn } from 'utils'
@@ -27,7 +27,7 @@ function Booking({ booking }: Props) {
       key={booking._id.toString()}
       className="flex relative gap-3 py-3 items-center"
     >
-      {/* <Qr booking={booking} /> */}
+      <Qr booking={booking} />
       <div className="pl-1 flex flex-col gap-1">
         <div className="flex gap-2 divide-x text-lg font-semibold divide-neutral-700">
           <span className="capitalize">{booking.table.floor.headquarder}</span>
