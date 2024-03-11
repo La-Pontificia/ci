47e69ea77d6a9d7b88638ce2892c5b92d0c3ea2b
Daustinn <daustinn.dev@gmail.com>
2024-03-11 11:42:03 -0500

    🪶 Add in Portal component the nadvar

--- src/components/workspace/floors/floor/nav/index.tsx
+++ src/components/workspace/floors/floor/nav/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { ItemNav } from './item'
 import { useUI } from 'stores'
+import { Portal } from 'commons/portal'
 
 type Props = {
   slug: string
@@ -11,18 +12,20 @@ type Props = {
 function FloorNav({ slug }: Props) {
   const isEditing = useUI((store) => store.isEditing)
   return (
-    <nav
-      aria-hidden={isEditing}
-      className="fixed aria-hidden:opacity-25 aria-hidden:pointer-events-none z-40 top-0 left-[50%] translate-x-[-50%]"
-    >
-      <div className="flex text-lg justify-center">
-        <ItemNav href={`/workspace/floors/${slug}`} title="Mesas" />
-        <ItemNav
-          href={`/workspace/floors/${slug}/reservations`}
-          title="Reservaciones"
-        />
-      </div>
-    </nav>
+    <Portal isVisible>
+      <nav
+        aria-hidden={isEditing}
+        className="fixed  aria-hidden:opacity-25 aria-hidden:pointer-events-none z-50 top-1 left-[50%] translate-x-[-50%]"
+      >
+        <div className="flex text-lg justify-center">
+          <ItemNav href={`/workspace/floors/${slug}`} title="Mesas" />
+          <ItemNav
+            href={`/workspace/floors/${slug}/reservations`}
+            title="Reservaciones"
+          />
+        </div>
+      </nav>
+    </Portal>
   )
 }
 
