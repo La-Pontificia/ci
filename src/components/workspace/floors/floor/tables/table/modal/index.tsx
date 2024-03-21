5d8823a54983af84fff61b5a00f1486ed03a07d6
Daustinn <daustinn.dev@gmail.com>
2024-03-20 20:19:33 -0500

    Update table modal layout

--- src/components/workspace/floors/floor/tables/table/modal/index.tsx
+++ src/components/workspace/floors/floor/tables/table/modal/index.tsx
@@ -7,12 +7,15 @@ type Props = { table: NewTypeTable; isReserved: boolean }
 
 export default function ModalContent({ table, isReserved }: Props) {
   return (
-    <div className="w-full relative max-700:min-h-[auto] min-h-[500px] h-full flex flex-col divide-neutral-800 p-4">
+    <div className="w-full max-w-[31rem] mx-auto relative max-700:min-h-[auto] min-h-[500px] h-full flex flex-col divide-neutral-800 p-4 pt-0">
       {isReserved && (
         <div className="absolute z-[1] inset-0 bg-white/60 font-semibold text-xl grid place-content-center">
           Mesa reservado
         </div>
       )}
+      <h2 className="text-center pb-1 -mt-1.5 text-lg font-semibold">
+        {table.name}
+      </h2>
       <ChairList table={table} />
       <MultipleUsers table={table} />
     </div>
