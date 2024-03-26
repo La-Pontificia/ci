5a88bbce566a0813443a5af9b776382a91989184
Daustinn <daustinn.dev@gmail.com>
2024-03-25 19:01:51 -0500

    Add responsible field to record creation

--- src/app/api/records/pc/per-user/route.ts
+++ src/app/api/records/pc/per-user/route.ts
@@ -1,12 +1,17 @@
+import { authOptions } from 'libs/next-auth'
 import { getTable, getUserById } from 'libs/server'
 import { createRecord } from 'libs/server/record'
 import { ObjectId } from 'mongodb'
+import { getServerSession } from 'next-auth'
 import { type NextRequest, NextResponse } from 'next/server'
 import { type Record } from 'types/record'
 import { z } from 'zod'
 
 export async function POST(req: NextRequest) {
   try {
+    const responsible = await getServerSession(authOptions)
+    if (!responsible?.account) throw new Error('Unauthorized')
+
     const data = await req.json()
     const parsedData = zod.safeParse(data)
     if (!parsedData.success) throw new Error('Invalid data')
@@ -15,14 +20,16 @@ export async function POST(req: NextRequest) {
 
     if (!table) throw new Error('Table not found')
 
-    const isNotComplet = new Date(table.current_users[0].to) < new Date()
-
+    const isNotComplet =
+      new Date(table.current_users[0].to) < new Date() ||
+      new Date(table.current_users[0].to) > new Date()
     const user = await getUserById(new ObjectId(parsedData.data.user_id))
 
     if (!user) throw new Error('User not found')
 
     const constructRecord: Record = {
       _id: new ObjectId(),
+      responsible: responsible.account,
       created_at: new Date(),
       table: {
         _id: new ObjectId(table?._id),
@@ -37,6 +44,7 @@ export async function POST(req: NextRequest) {
         from: table.current_users[0].from,
         to: isNotComplet ? new Date() : table.current_users[0].to,
         user: {
+          dni: user.dni,
           _id: new ObjectId(user._id),
           career: user.career,
           sex: user.sex,
