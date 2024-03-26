82c277f350d389f0e76ad9e17740bd9b24c51c5d
Daustinn <daustinn.dev@gmail.com>
2024-03-25 19:03:02 -0500

    Update sidebar width in workspace component

--- src/components/workspace/sidebar/index.tsx
+++ src/components/workspace/sidebar/index.tsx
@@ -4,7 +4,7 @@ import { Chart2Icon, TableIcon, Users2Icon } from 'icons'
 
 export default function Sidebar() {
   return (
-    <aside className="w-60 fixed h-[calc(100svh-4rem)] flex flex-col border-r p-2 px-3 border-neutral-200 bg-white">
+    <aside className="w-28 fixed h-[calc(100svh-4rem)] flex flex-col border-r p-2 px-3 border-neutral-200 bg-white">
       <nav className="font-medium flex-grow space-y-1 flex flex-col w-full">
         <ItemTab
           icon={<TableIcon className="scale-110" />}
