78cd85f4f48f5863faa3a70cefaf26cdad0a5fbd
Daustinn <daustinn.dev@gmail.com>
2024-03-26 11:27:54 -0500

    Refactor Suspense component in Records page

--- src/app/(nav)/workspace/(only-works)/reports/records/page.tsx
+++ src/app/(nav)/workspace/(only-works)/reports/records/page.tsx
@@ -15,21 +15,7 @@ export default function Records({
   return (
     <div className="p-2">
       <Nav />
-      <Suspense
-        key={`report-${searchParams.from} ${searchParams.to}`}
-        fallback={
-          <div className="p-4">
-            <div className="w-full grid grid-cols-3">
-              <div className="bg-stone-200 animate-pulse col-span-3 rounded-2xl h-[250px] " />
-            </div>
-            <div className="grid-cols-4 max-1500:grid-cols-2 max-900:grid-cols-1 grid pt-4 gap-3">
-              <div className="bg-stone-200 animate-pulse rounded-2xl h-[400px] " />
-              <div className="bg-stone-200 animate-pulse rounded-2xl h-[400px] " />
-              <div className="bg-stone-200 animate-pulse rounded-2xl h-[400px] " />
-            </div>
-          </div>
-        }
-      >
+      <Suspense>
         <RecordsReports searchParams={searchParams} />
       </Suspense>
     </div>
