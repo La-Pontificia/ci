93695fe1c9433ce15dd58ef3ffef434f13cf6a65
Daustinn <daustinn.dev@gmail.com>
2024-03-27 15:56:41 -0500

    Update header logo styling

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -4,14 +4,13 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header className="flex fixed pointer-events-none inset-x-0 z-10 w-full mx-auto">
+    <header className="flex fixed pointer-events-none inset-x-0 z-20 w-full mx-auto">
       <picture className="p-5 pointer-events-auto">
         <div className="w-[140px]">
           <Image
             placeholder="empty"
             src="/white-logo.png"
-            loading="lazy"
-            className="opacity-80 w-full h-full"
+            className="w-full h-full"
             alt="Escuela nega Superior La Pontificia - Logo"
             height={60}
             width={140}
