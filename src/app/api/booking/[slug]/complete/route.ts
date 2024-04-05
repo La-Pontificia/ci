1a2eef4bc50aca98b889aa3b59aaa70e7dab762d
Daustinn <daustinn.dev@gmail.com>
2024-04-05 16:50:00 -0500

    Add table update and complete booking functionality

--- src/app/api/booking/[slug]/complete/route.ts
+++ src/app/api/booking/[slug]/complete/route.ts
@@ -1,3 +1,4 @@
+import { getTable, updateTable } from 'libs/server'
 import { updateBooking } from 'libs/server/booking'
 import { removeReservedDateByBooking } from 'libs/server/bookings/utils'
 import { NextResponse, type NextRequest } from 'next/server'
@@ -10,12 +11,42 @@ export async function PATCH(
     const id = params.slug
 
     const { booking } = await removeReservedDateByBooking(id)
+
+    const table = await getTable(booking.table._id.toString())
+    if (!table) return NextResponse.json({ status: 500 })
+
+    const newCurrentUsers: typeof table.current_users = booking.users.map(
+      (e, i) => ({
+        chair: i + 1,
+        display_time: booking.time,
+        from: booking.from,
+        time: booking.time,
+        to: booking.to,
+        user: {
+          _id: e._id,
+          email: e.email,
+          image: e.image,
+          names: e.names
+        }
+      })
+    )
+
+    // Update table
+    await updateTable(
+      {
+        current_users: newCurrentUsers
+      },
+      table._id
+    )
+
+    // Update booking
     await updateBooking(
       {
         status: 'completed'
       },
       booking._id
     )
+
     return NextResponse.json(id, { status: 200 })
   } catch (error) {
     console.error(error)
