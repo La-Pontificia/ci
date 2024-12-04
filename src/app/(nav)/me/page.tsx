188a8fd06737fb75c8ed4da031db23531e403370
Daustinn <daustinn.music@gmail.com>
2024-12-04 09:21:18 -0500

    refactor: Add metadata generation for reservations page

--- src/app/(nav)/me/page.tsx
+++ src/app/(nav)/me/page.tsx
@@ -1,5 +1,17 @@
 import Booking from 'components/me/booking'
+import { authOptions } from 'libs/next-auth'
+import { getServerSession } from 'next-auth'
 import React from 'react'
+
+export async function generateMetadata() {
+  const session = await getServerSession(authOptions)
+  return {
+    title:
+      session?.account?.names +
+      ' - Reservas | Centro de Información La Pontificia',
+    icons: session?.account?.image ? [session?.account?.image] : []
+  }
+}
 
 function MePage() {
   return <Booking />
