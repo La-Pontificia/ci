5804dcda79f31dfa0fe54fcc6dc09ebca540eb43
Daustinn <daustinn.music@gmail.com>
2024-12-04 09:21:13 -0500

    refactor: Add metadata for reservations page

--- src/app/(nav)/floors/page.tsx
+++ src/app/(nav)/floors/page.tsx
@@ -1,6 +1,10 @@
 import Floors from 'components/floors'
 import React from 'react'
 
+export const metadata = {
+  title: 'Pisos y sedes | Centro de Informacion La Pontificia'
+}
+
 function page() {
   return (
     <div className="p-10">
